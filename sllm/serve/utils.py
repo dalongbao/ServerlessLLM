628c45cc105bc26385dea0f09c094cba56552e5e
tsuiusi <ryantsui786@gmail.com>
2025-07-01 20:18:04 +0100

    fix: added logger to utils

--- sllm/serve/utils.py
+++ sllm/serve/utils.py
@@ -21,6 +21,9 @@ from typing import List, Optional
 
 import ray
 
+from sllm.serve.logger import init_logger
+
+logger = init_logger(__name__)
 
 def get_worker_nodes():
     ray_nodes = ray.nodes()
